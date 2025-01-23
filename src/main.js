0b6031257626940c091fbf2ded0d93559e02df2a
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2025-01-23 17:09:11 +0100

    feat: chart section

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import "./css/heresey.css";
 import "./css/regia.css";
 import "./css/printer.css";
 import "./css/fury.css";
+import "./css/chart.css";
 import handleMenu from "./js/handleMenu";
 
 handleMenu();
