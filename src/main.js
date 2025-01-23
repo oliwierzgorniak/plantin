737ca2db29a82f45d1636e396ee177146042b063
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2025-01-23 10:59:02 +0100

    feat: regia section

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import handleMenu from "./js/handleMenu";
 import "./css/main.css";
 import "./css/nav.css";
 import "./css/hero.css";
 import "./css/market.css";
 import "./css/heresey.css";
+import "./css/regia.css";
+import handleMenu from "./js/handleMenu";
 
 handleMenu();
