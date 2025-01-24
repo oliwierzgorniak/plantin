8b39481e77107da8444bf9718487b8886e20430d
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2025-01-24 19:19:07 +0100

    feat: paper background addded to few sections

--- src/js/handleHero.js
+++ src/js/handleHero.js
@@ -5,16 +5,26 @@ const hadnleHero = () => {
     scrollTrigger: {
       trigger: ".hero",
       start: "top top",
-      // end: "+=1000",
+      end: "+=1000",
       scrub: true,
       pin: true,
     },
   });
 
+  tl.fromTo(".hero__circle-img", { y: "120%", ease: "power2.out" }, { y: 0 });
+
   tl.fromTo(
-    ".hero__message",
+    ".hero__message1",
     {
       y: "70vh",
+    },
+    { y: "-80vh" }
+  );
+
+  tl.fromTo(
+    ".hero__message2",
+    {
+      y: "30vh",
     },
     { y: "-100vh" }
   );
