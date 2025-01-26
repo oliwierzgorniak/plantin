af89588cc7afab257ef5506ca24e7b7e04c189e4
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2025-01-26 01:04:18 +0100

    fix: leiden almost working on various devices, except tablet

--- src/js/handleLeiden/handleSlider.js
+++ src/js/handleLeiden/handleSlider.js
@@ -67,6 +67,86 @@ const handleSlider = () => {
       });
     }
   });
+
+  $phone.addEventListener("click", () => {
+    isPressed = true;
+  });
+
+  $phone.addEventListener("mousemove", (e) => {
+    if (!isPressed) return;
+
+    const x = e.clientX;
+    // asked AI for how to get the offset of the container
+    const containerSpecs = $sliderContainer.getBoundingClientRect();
+    const containerWidthString =
+      window.getComputedStyle($sliderContainer).width;
+    const containerWidth = +containerWidthString.substring(
+      0,
+      containerWidthString.length - 2
+    );
+    const offset = containerSpecs.left;
+    let translateX = x - offset - $phone.offsetWidth / 2;
+    translateX = translateX < 0 ? 0 : translateX;
+    translateX =
+      translateX > containerWidth - $phone.offsetWidth
+        ? containerWidth - $phone.offsetWidth
+        : translateX;
+
+    $phone.style.transform = `translateX(${translateX}px)`;
+
+    if (translateX == containerWidth - $phone.offsetWidth) {
+      const $plantinPin = document.querySelector(".leiden__plantin-pin");
+      const tl = gsap.timeline();
+
+      const $container = document.querySelector(".leiden__call-container");
+      tl.to($container, {
+        opacity: 0,
+        ease: "power2.out",
+        onComplete: () => {
+          $container.classList.add("hidden");
+        },
+      });
+      let mm = gsap.matchMedia();
+      mm.add("(max-width: 799px)", () => {
+        tl.to($plantinPin, {
+          x: () => {
+            const containerWidth = document.querySelector(
+              ".leiden__img-container"
+            ).offsetWidth;
+            const xPos = containerWidth * 0.13;
+            return xPos - $plantinPin.offsetLeft;
+          },
+          y: () => {
+            const containerHeight = document.querySelector(
+              ".leiden__img-container"
+            ).offsetHeight;
+            const yPos = containerHeight * 0.4;
+            return yPos - $plantinPin.offsetTop;
+          },
+          ease: "power2.out",
+        });
+      });
+      mm.add("(min-width: 800px)", () => {
+        tl.to($plantinPin, {
+          x: () => {
+            const containerWidth = document.querySelector(
+              ".leiden__img-container"
+            ).offsetWidth;
+            const xPos = containerWidth * 0.46;
+            return xPos - $plantinPin.offsetLeft;
+          },
+          y: () => {
+            const containerHeight = document.querySelector(
+              ".leiden__img-container"
+            ).offsetHeight;
+            const yPos = containerHeight * 0.285;
+            return yPos - $plantinPin.offsetTop;
+          },
+          ease: "power2.out",
+        });
+      });
+    }
+  });
 };
 
 export default handleSlider;
